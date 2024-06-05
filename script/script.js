fd207701669119e652e536a385d723e8d7c10ad2
MISHOW2 <mshibambo63@gmail.com>
2024-06-05 04:41:35 +0200

    made website responsive for smaller screens

--- script/script.js
+++ script/script.js
@@ -1,16 +1,8 @@
-let lastScrollTop = 0;
-const header = document.querySelector('.header');
-const headerHeight = header.offsetHeight;
+document.addEventListener('DOMContentLoaded', function() {
+  const navToggle = document.getElementById('navToggle');
+  const navMenu = document.getElementById('navMenu');
 
-window.addEventListener('scroll', function() {
-  let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (currentScroll > lastScrollTop && currentScroll > headerHeight) {
-    // Scroll down, hide the header
-    header.style.transform = 'translateY(-100%)';
-  } else {
-    // Scroll up or at the top, show the header
-    header.style.transform = 'translateY(0)';
-  }
-  lastScrollTop = currentScroll;
+  navToggle.addEventListener('click', function() {
+      navMenu.classList.toggle('open');
+  });
 });
